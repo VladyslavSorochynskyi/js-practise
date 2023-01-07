cddd664eb9f934a44a09b7a67ac96feb30ea084a
VladyslavSorochynskyi <114526607+VladyslavSorochynskyi@users.noreply.github.com>
2023-01-07 22:55:07 +0200

    +

---
+++ autocheck.js
@@ -0,0 +1,40 @@
+'use stick';
+
+//1 - 2
+//let productName = 'Droid';
+//let pricePerItem = 2000;
+
+//productName = 'Repair droid';
+//pricePerItem = 3500;
+
+//console.log(productName, pricePerItem);
+
+//3
+//let topSpeed = 160;
+//let distance = 617.54;
+//let login = 'mango935'
+//let isOnline = true;
+//let isAdmin = false;
+
+//4
+//const pricePerItem = 3500;
+//const orderedQuantity = 4;
+//const totalPrice = pricePerItem * orderedQuantity;
+//console.log(totalPrice);
+
+//5
+//const productName = 'Droid';
+//const pricePerItem = 3500;
+//const message = 'You picked ${productName},price per item is ${pricePerItem} credits';
+//console.log(message);
+
+//6
+//const pricePerItem = 800;
+//const orderedQuantity = 6;
+//const deliveryFee = 50;
+//const totalPrice = pricePerItem * orderedQuantity + deliveryFee;
+//const message = 'You ordered droids worth ${ totalPrice } credits.Delivery (${ deliveryFee } credits) is included in total price.';
+
+//7
+
+
---
+++ test.js
@@ -0,0 +1,28 @@
+
+/* Строгий режим*/
+'use strick';
+/* Comment */
+
+
+/*=============================================
+   1)     Как работает модалка с подтверждением confirm 
+          true - ok     false - cancel
+
+    2)    Модалка со строкой для данных promt
+            ok - значение     cancel - null
+=============================================*/
+//const shouldRenew = confirm('Желаете продлить подписку');
+//console.log(shouldRenew);
+
+//const quantity = prompt("Введите количество");
+//console.log(quantity);
+/*=====  End of Section comment block  ======*/
+
+
+
+
+
+
+
+
+
